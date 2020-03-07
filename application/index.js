686881b827bd3ebcf51f87a27bfc46051fde9a8e
Enrique yepez <enriqueyt@gmail.com>
2020-03-07 17:57:26 +0000

    N - added application server
    N - added db config
    
    N - added configuration file

---
+++ application/index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const config = require('../config');
+
+const router = require('./router');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
+
+app.use(express.static(path.join(__dirname, '..', 'public')));
+
+app.use(router);
+
+/**
+ * entry point
+ */
+if (require.main === module) {
+    app.listen(config.port, () => {
+        console.log(__dirname);
+        console.log('app listening on port', config.port);
+    });
+}
+
+module.exports = app;
---
+++ config/index.js
@@ -0,0 +1,6 @@
+const path = require('path');
+const port = parseInt(process.env.PORT || 3000);
+const dbName = process.env.DB_PATH || 'db.test.json';
+
+module.exports.port = port;
+module.exports.dbPath = path.resolve(__dirname, dbName);
